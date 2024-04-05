5f313e872e2368ed56aa59b0c0acacfd9d1be6dd
mtaut <146488331+mtaut@users.noreply.github.com>
2024-04-05 17:17:44 -0600

    Server update
    Corrected spelling.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require("http");
-const url = require("url");
 const fs = require("fs");
+const url = require("url");
 
 http
   .createServer((request, response) => {
@@ -13,6 +13,18 @@ http
       filePath = __dirname + "/index.html";
     }
 
+    fs.appendFile(
+      "log.txt",
+      "URL: " + request.url + "\nTimestamp: " + new Date() + "\n\n",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Added to log.");
+        }
+      }
+    );
+
     fs.readFile(filePath, (err, data) => {
       if (err) {
         response.writeHead(404, { "Content-Type": "text/html" });
@@ -23,22 +35,6 @@ http
       }
       response.end();
     });
-
-    fs.appendFile(
-      "log.txt",
-      "URL: " + request.url + " - Timestamp: " + new Date() + "\n\n",
-      (err) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Added to log.");
-        }
-      }
-    );
   })
   .listen(5500);
-console.log("Server is running on Port 5500.");
-
-console.log(q.host);
-console.log(q.pathname);
-console.log(q.search);
+console.log("Server is running on Port 5500. Log, do you read this?");
