8fa4e0ddfc8360c9e2095b4f7160dc2a5587f2b5
mtaut <146488331+mtaut@users.noreply.github.com>
2024-04-11 11:29:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,59 @@
 const express = require("express");
 const app = express();
+
+let topMovies = [
+  {
+    title: "The Lives of Others",
+    director: "Florian Henckel von Donnersmarck",
+  },
+  {
+    title: "Snatch",
+    director: "Guy Ritchie",
+  },
+  {
+    title: "The Matrix",
+    director: "Lana Wachowski, Lilly Wachowski",
+  },
+  {
+    title: "Fight Club",
+    director: "David Fincher",
+  },
+  {
+    title: "Hot Fuzz",
+    director: "Edgar Wright",
+  },
+  {
+    title: "Casino Royale",
+    director: "Martin Campbell",
+  },
+  {
+    title: "Cool Runnings",
+    director: "Jon Turtelaub",
+  },
+  {
+    title: "Good Will Hunting",
+    director: "Gus Van Sant",
+  },
+  {
+    title: "Black Panther",
+    director: "Ryan Coogler",
+  },
+  {
+    title: "Grand Budapest Hotel",
+    direcor: "Wes Anderson",
+  },
+];
+
+// GET requests
+app.get("/", (req, res) => {
+  res.send("Welcome to myFlix movie app!");
+});
+
+app.get("/movies", (req, res) => {
+  res.json(topMovies);
+});
+
+// listen for requests
+app.listen(5500, () => {
+  console.log("Your app is listening on port 5500.");
+});
