969a890bb382d370ded87f3801efb49a974f70a3
mtaut <146488331+mtaut@users.noreply.github.com>
2024-04-26 12:14:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+const express = require("express");
 const bodyParser = require("body-parser");
 const nodemon = require("nodemon");
 const uuid = require("uuid");
 const app = express();
 const mongoose = require("mongoose");
 const Models = require("./models.js");
-const express = require("express");
 
 const Movies = Models.Movie;
 const Users = Models.User;
