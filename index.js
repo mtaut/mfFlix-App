b05f968fb48d0e256f2ee989d761d742a94796f4
mtaut <146488331+mtaut@users.noreply.github.com>
2024-05-04 13:48:00 -0600

    Adding hashing

--- index.js
+++ index.js
@@ -256,14 +256,16 @@ require("./passport");
 
 // CREATE, allow users to register  *** EXCLUDE THIS ENDPOINT FOR PASSPORT STRATEGIES ***
 app.post("/users", async (req, res) => {
-  await Users.findOne({ Username: req.body.Username })
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
     .then((user) => {
       if (user) {
+        // If the user is found, send a response that it already exists
         return res.status(400).send(req.body.Username + "already exists");
       } else {
         Users.create({
           Username: req.body.Username,
-          Password: req.body.Password,
+          Password: hashedPassword,
           Email: req.body.Email,
           Birthday: req.body.Birthday,
         })
