b69abf90c3a888bfe69df3087dcf1b17e946f3f3
mtaut <146488331+mtaut@users.noreply.github.com>
2024-05-31 11:02:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -226,16 +226,20 @@ app.delete(
 );
 
 // READ will return full list of movies
-app.get("/movies", async (req, res) => {
-  Movies.find()
-    .then((movies) => {
-      res.status(201).json(movies);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/movies",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    Movies.find()
+      .then((movies) => {
+        res.status(201).json(movies);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 // READ get movie by title
 app.get(
